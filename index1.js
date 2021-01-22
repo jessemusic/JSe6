bc86ca5db91b61cf336ca575b9a50547fa84bbf2
jessemusic <jmc_cursos@hotmail.com>
2021-01-22 19:51:32 -0300

    Construção de Promisses simples

--- index1.js
+++ index1.js
@@ -1,7 +1,11 @@
+// ES7 - Async / Await
 const simpleFunc = async() => {
+    throw new Error('On no!');
     return 12345;
 };
 simpleFunc().then(data => {
     console.log(data);
 
+}).catch(err => {
+    console.log(err);
 });
