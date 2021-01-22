a134a362679081bf171651e5dbc972ea0d4d36b8
jessemusic <jmc_cursos@hotmail.com>
2021-01-21 21:04:22 -0300

    Agora mais limpo o código com o mesmo tratamento de errro

--- index.js
+++ index.js
@@ -15,51 +15,12 @@ const doOtherThingPromise = () => new Promise((resolve, reject) => {
 //console.log(doSomeThingPromise);
 doSomeThingPromise()
     .then(data => {
-        console.log(data);
+        console.log(data.split(''));
         return doOtherThingPromise();
     })
-    .then(data2 => console.log(data2))
+    .then(data2 => console.log(data2.split('')))
     .catch(error => console.log('Ops', error));
 
-
-
-// Callbacks
-function doSomeThing(callback) {
-    setTimeout(function() {
-        //did something
-        callback('First data');
-    }, 1000);
-}
-
-function doOtherThing(callback) {
-    setTimeout(function() {
-        //did othersomething
-        callback('Second data');
-    }, 1000);
-}
-
-function doAll() {
-    try {
-        doSomeThing(function(data) {
-            var processedData = data.split('');
-
-            try {
-                doOtherThing(function(data2) {
-                    var processedData2 = data2.split('');
-                    try {
-                        setTimeout(function() {
-                            console.log(processedData, processedData2);
-                        }, 1000);
-                    } catch (err) {
-                        //handle moreInside
-                    }
-                });
-            } catch (err) {
-                // handle error inside
-            }
-        });
-    } catch (err) {
-        // handle error
-    }
-}
-doAll();
+//Pending
+//Fulfilled
+//Rejected
