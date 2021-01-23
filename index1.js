14537fbcafceeddf981f22af0104bdfa4e30c212
jessemusic <jmc_cursos@hotmail.com>
2021-01-22 21:44:46 -0300

    Async Await e requisição em paralelo

--- index1.js
+++ index1.js
@@ -1,11 +1,22 @@
 // ES7 - Async / Await
-const simpleFunc = async() => {
-    throw new Error('On no!');
-    return 12345;
-};
-simpleFunc().then(data => {
-    console.log(data);
-
-}).catch(err => {
-    console.log(err);
+const asyncTimer = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('12345');
+    }, 1000);
 });
+
+const simpleFunc = async() => {
+    const data = await Promise.all([
+        asyncTimer().
+        fetch('/data.json').then(resStream =>
+            resStream.json())
+    ]);
+    return data;
+};
+simpleFunc()
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
